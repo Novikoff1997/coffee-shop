e74755beadc7b16a8b78b5448b17229dad643dd1
Novikoff1997 <andreytoys@yandex.ru>
2025-04-12 01:14:54 +0300

    Задание к уроку 3

---
+++ src/router/index.js
@@ -0,0 +1,27 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+import HeroView from "@/views/HeroView.vue";
+import OurCoffeeView from "@/views/OurCoffeeView.vue";
+import ForYourPleasure from "@/views/ForYourPleasure.vue";
+import CountactUs from "@/views/CountactUs.vue";
+import ThanksView from "@/views/ThanksView.vue";
+import ProductView from "@/views/ProductView.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  { path: "/", component: HeroView },
+  { path: "/our-coffee", component: OurCoffeeView },
+  { path: "/for-your-pleasure", component: ForYourPleasure },
+  { path: "/contact-us", component: CountactUs },
+  { path: "/thanks", component: ThanksView },
+  { path: "/product", component: ProductView },
+];
+
+const router = new VueRouter({
+  mode: "history",
+  routes,
+});
+
+export default router;
